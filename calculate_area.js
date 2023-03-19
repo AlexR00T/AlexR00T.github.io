2e7b4f140a655ec9168e2756d9f64615cc2812ff
Alexr00t <117912943+Alexr00t@users.noreply.github.com>
2023-03-20 01:56:01 +0200

    Initial commit

--- calculate_area.js
+++ calculate_area.js
@@ -1,8 +1,50 @@
-function initMap(map) {
+window.initMap = function(map) {
   // Load solar farm data
   fetch("solar_farms_data.json")
     .then((response) => response.json())
     .then((data) => {
+      // Create marker cluster group
+      const markerClusterGroup = L.markerClusterGroup({
+        iconCreateFunction: function(cluster) {
+          const markers = cluster.getAllChildMarkers();
+          let totalCapacity = 0;
+
+          markers.forEach(marker => {
+            totalCapacity += marker.options.capacity;
+          });
+
+          // Convert to MW or GW and format the string
+          let capacityString;
+          if (totalCapacity < 1000) {
+            capacityString = `${totalCapacity.toFixed(0)} kW`;
+          } else {
+            totalCapacity /= 1000;
+            capacityString = `${totalCapacity.toFixed(1)} MW`;
+            if (totalCapacity >= 1000) {
+              totalCapacity /= 1000;
+              capacityString = `${totalCapacity.toFixed(2)} GW`;
+            }
+          }
+
+          return L.divIcon({
+            html: `<div class="cluster-capacity">${capacityString}</div>`,
+            className: 'marker-cluster',
+            iconSize: L.point(20, 20)
+          });
+        }
+      });
+
+      // Add markers to the cluster group
+      data.forEach((solarFarm) => {
+        const latLng = new L.LatLng(solarFarm[0][0], solarFarm[0][1]);
+        const capacity = solarFarm[1] * 0.15; // 150 watts per square meter converted to kW
+        const marker = L.marker(latLng, { capacity: capacity });
+        markerClusterGroup.addLayer(marker);
+      });
+
+      // Add the cluster group to the map
+      map.addLayer(markerClusterGroup);
+
       // Function to calculate the total solar farm area within the map view
       function calculateTotalArea() {
         const bounds = map.getBounds();
@@ -16,20 +58,20 @@ function initMap(map) {
             totalCapacity += solarFarm[1] * 0.15; // 150 watts per square meter converted to kW
           }
         });
-		
-	function formatNumber(number) {
-		return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-	}	
 
-        document.getElementById("totalArea").innerText = `Total PV Area:${formatNumber(totalArea.toFixed(0))} m2`; 
+        function formatNumber(number) {
+          return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        }
+
+        document.getElementById("totalArea").innerText = `Total PV Area:${formatNumber(totalArea.toFixed(0))} m2`;
 
         // Convert to MW or GW and format the string
         let capacityString;
         if (totalCapacity < 1000) {
-          capacityString = `${totalCapacity.toFixed(2)} kW`;
+          capacityString = `${totalCapacity.toFixed(0)} kW`;
         } else {
           totalCapacity /= 1000;
-          capacityString = `${totalCapacity.toFixed(2)} MW`;
+          capacityString = `${totalCapacity.toFixed(1)} MW`;
           if (totalCapacity >= 1000) {
             totalCapacity /= 1000;
             capacityString = `${totalCapacity.toFixed(2)} GW`;
